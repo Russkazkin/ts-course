8e909916795b1f878b645201f1343bb537d6aa6b
Russkazkin <ruslan@skazkin.su>
2021-04-24 14:43:09 +0300

    Finish basic types

---
+++ basic_types.ts
@@ -0,0 +1,19 @@
+enum Status { ACTIVE }
+
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+  active: number
+} = {
+  name: 'Ruslan',
+  age: 45,
+  hobbies: ['Games', 'Coding'],
+  role: [2, 'author'],
+  active: Status.ACTIVE,
+};
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
