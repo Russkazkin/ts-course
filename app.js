0c686737c7c923c184e40a16e26828bf34a1e2a3
Russkazkin <ruslan@skazkin.su>
2021-05-10 14:45:27 +0300

    Finish extra types

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Let\'s learn more types!');
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Ruslan';
+if (typeof userInput === 'string') {
+    userName = userInput;
+}
+var generateError = function (message, code) {
+    throw { message: message, errorCode: code };
+};
+generateError('Error accurred!', 500);
