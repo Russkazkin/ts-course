d7d48ae57e4120167ebb0ca40b596d25bbf36886
Russkazkin <ruslan@skazkin.su>
2021-05-13 18:13:28 +0300

    Save unknown and never types lesson

--- app.js
+++ app.js
@@ -1,11 +1,1 @@
 "use strict";
-var userInput;
-var userName;
-userInput = 'Ruslan';
-if (typeof userInput === 'string') {
-    userName = userInput;
-}
-var generateError = function (message, code) {
-    throw { message: message, errorCode: code };
-};
-generateError('Error accurred!', 500);
