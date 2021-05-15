79b132e027bd085635cbe6c76df0b2cf3f8fa1d1
Russkazkin <ruslan@skazkin.su>
2021-05-15 06:41:09 +0300

    Set noEmitOnError to prevent compilation with errors in ts

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,8 @@
 "use strict";
 var map = new Map();
-map.set('foo', 'bar');
+map.set('success', 'Successful Execution.');
+map.set('error', 'An Error Occurred while Processing.');
+var button = document.querySelector('button');
+button.addEventListener('click', function () { return console.log(map.get('success')); });
 console.log(map);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,9 @@
-const map = new Map();
-map.set('foo', 'bar');
+const map: Map<string, string> = new Map();
+map.set('success', 'Successful Execution.');
+map.set('error', 'An Error Occurred while Processing.');
+
+const button = document.querySelector('button')!;
+
+button.addEventListener('click', () => console.log(map.get('success')));
+
 console.log(map);
